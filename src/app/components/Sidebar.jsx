f9899aaa151c985ea93aa24c6d6a2f49fb5dcbfa
Fuad Abou Heit <f.abouheit@gmail.com>
2024-07-16 19:05:07 +0300

    add mini profile to sidebar component

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { HiHome } from "react-icons/hi";
+import { HiHome, HiDotsHorizontal } from "react-icons/hi";
 import { FaXTwitter } from "react-icons/fa6";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
@@ -8,32 +8,47 @@ import { signIn, signOut, useSession } from "next-auth/react";
 export default function Sidebar() {
   const {data: session} = useSession();
   return (
-    <div className="flex flex-col gap-4 p-3">
+    <div className="flex flex-col p-3 justify-between h-screen">
+      <div className="flex flex-col gap-4">
+
       <Link href="/">
         <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
       </Link>
       <Link
         href=""
         className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit"
-      >
+        >
         <HiHome className="w-7 h-7" />
         <span className="font-bold hidden xl:inline">Home</span>
       </Link>
       {session ? (
         <button
-          className="bg-blue-400 text-white rounded-full hover:brightness-95 font-semibold transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
-          onClick={() => signOut()}
+        className="bg-blue-400 text-white rounded-full hover:brightness-95 font-semibold transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+        onClick={() => signOut()}
         >
           Sign Out
         </button>
       ) : (
         <button
-          className="bg-blue-400 text-white rounded-full hover:brightness-95 font-semibold transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
-          onClick={() => signIn()}
+        className="bg-blue-400 text-white rounded-full hover:brightness-95 font-semibold transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+        onClick={() => signIn()}
         >
           Sign In
         </button>
       )}
+      </div>
+      {
+        session && (
+          <div className="text-gray-700 text-sm flex items-center cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200">
+            <img src={session.user.image} alt="user profile picture" className="h-10 w-10 rounded-full xl:mr-2"/>
+            <div className="hidden xl:inline">
+            <h4 className="font-bold">{session.user.name}</h4>
+            <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <HiDotsHorizontal className="h-5 xl:ml-8 hidden xl:inline"/>
+          </div>
+        )
+      }
     </div>
   );
 }

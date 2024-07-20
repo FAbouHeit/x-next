95576299d9e6d7be3236c0c75f5910b971f99d7c
Fuad Abou Heit <f.abouheit@gmail.com>
2024-07-20 15:57:53 +0300

    minor fix to deploy

--- src/app/components/News.jsx
+++ src/app/components/News.jsx
@@ -17,7 +17,7 @@ export default function News() {
 
   return (
     <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 transition-all duration-200">
-      <h4 className="font-bold text-xl px-4">What's happening</h4>
+      <h4 className="font-bold text-xl px-4">What is happening</h4>
       {news.slice(0, articleNum).map((article)=>{
         return (
         <>
